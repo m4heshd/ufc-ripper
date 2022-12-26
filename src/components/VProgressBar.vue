20be2b83a0858848f2c761bc02dc13577155fde9
Mahesh Bandara Wijerathna <m4heshd@gmail.com>
2022-12-26 14:46:12 +0530

    Create `VProgressBar` component

---
+++ src/components/VProgressBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="small-space border v-progress-bar">
+    <div class="v-progress-bar__progress"></div>
+  </div>
+</template>
+
+<script setup>
+// Core
+import {computed} from 'vue';
+
+const props = defineProps({
+  vProgress: {
+    type: Number,
+    default: 0
+  }
+});
+
+const progress = computed(() => `${props.vProgress}%`);
+</script>
+
+<style lang="scss">
+.v-progress-bar {
+  position: relative;
+
+  &__progress {
+    height: 100%;
+    width: v-bind(progress);
+    background: var(--active);
+  }
+}
+</style>
