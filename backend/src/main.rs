5e61f38a44d7bb1c0946c256efa0bfdd50b33b42
Mahesh Bandara Wijerathna <m4heshd@gmail.com>
2024-03-16 06:11:28 +0530

    [main, net_util] Create the initial server using `axum` for serving GUI

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Welcome to UFC Ripper in Rust!");
+// Modules
+mod net_util;
+
+// Libs
+use crate::net_util::init_server;
+
+#[tokio::main]
+async fn main() {
+    init_server().await;
 }
---
+++ backend/src/net_util.rs
@@ -0,0 +1,25 @@
+// Libs
+use axum::{http::StatusCode, routing::get_service, Router};
+use std::net::SocketAddr;
+use tokio::net::TcpListener;
+use tower_http::services::ServeDir;
+
+/// Creates a new server that serves the UFC Ripper GUI
+pub async fn init_server() {
+    let port = 8383;
+    let app = Router::new().nest_service(
+        "/",
+        get_service(ServeDir::new("./dist")).handle_error(|_| async move {
+            (StatusCode::INTERNAL_SERVER_ERROR, "internal server error")
+        }),
+    );
+    let listener = TcpListener::bind(SocketAddr::from(([0, 0, 0, 0], port)))
+        .await
+        .unwrap_or_else(|_| panic!("Failed to start a listener on the port \"{port}\"\n"));
+
+    axum::serve(listener, app)
+        .await
+        .expect("Failed to initiate the backend server\n");
+
+    println!("UFC Ripper GUI is live at http://localhost:${port}\n");
+}
