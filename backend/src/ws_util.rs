01eeb07fd8a227246356b64c66ff47626a770593
Mahesh Bandara Wijerathna <m4heshd@gmail.com>
2024-03-23 07:00:53 +0530

    [ws_util] Construct proper `UFCRError` in `send_error()`
    Other changes:
    - Remove the unnecessary `send_async_result()`

--- backend/src/ws_util.rs
+++ backend/src/ws_util.rs
@@ -2,7 +2,7 @@
 use crate::{
     app_util::{check_app_update, get_app_metadata},
     bin_util::validate_bins,
-    config_util::get_config,
+    config_util::{get_config, is_debug},
     net_util::{search_vods, JSON},
     state_util::get_dlq,
 };
@@ -13,7 +13,10 @@ use socketioxide::{
     layer::SocketIoLayer,
     SocketIoBuilder,
 };
-use std::{fmt::Display, future::Future, time::Duration};
+use std::{
+    fmt::{Debug, Display},
+    time::Duration,
+};
 
 /// Creates a new Tower layer with a `socket.io` server instance on the default namespace.
 pub fn create_ws_layer() -> SocketIoLayer {
@@ -40,7 +43,7 @@ fn handle_ws_client(socket: &SocketRef) {
         ack.send(get_dlq()).ok();
     });
     socket.on("check-app-update", |ack: AckSender| async move {
-        send_async_result(ack, check_app_update()).await;
+        send_result(ack, check_app_update().await);
     });
     socket.on("validate-media-tools", |ack: AckSender| {
         ack.send(validate_bins()).ok();
@@ -54,41 +57,48 @@ fn handle_ws_client(socket: &SocketRef) {
             if query.is_none() || page.is_none() {
                 send_error(ack, "Invalid search request");
             } else {
-                send_async_result(ack, search_vods(query.unwrap(), page.unwrap())).await;
+                send_result(ack, search_vods(query.unwrap(), page.unwrap()).await);
             }
         },
     );
-}
-
-/// Sends a response to the client-event with data or an error, according to the awaited `Result`.
-async fn send_async_result<T, E>(ack: AckSender, future: impl Future<Output = Result<T, E>>)
-where
-    T: Serialize,
-    E: Display,
-{
-    send_result(ack, future.await);
 }
 
 /// Sends a response to the client-event with data or an error, according to the `Result`.
 fn send_result<T, E>(ack: AckSender, result: Result<T, E>)
 where
     T: Serialize,
-    E: Display,
+    E: Display + Debug,
 {
     match result {
         Ok(data) => {
             ack.send(data).ok();
         }
         Err(error) => {
-            send_error(ack, &error.to_string());
+            send_error(ack, error);
         }
     }
 }
 
-/// Sends an error response to the client with the provided message
-fn send_error(ack: AckSender, error_msg: &str) {
+/// Constructs and sends an error response to the client with the provided error
+fn send_error<E>(ack: AckSender, error: E)
+where
+    E: Display + Debug,
+{
+    let error_dbg = format!("{error:#?}");
+    let error_msg = error.to_string();
+
+    if is_debug() {
+        log_err!("{error_dbg}");
+    } else {
+        log_err!("{error_msg}");
+    }
+
     ack.send(json!({
-        "error": error_msg
+        "error": {
+            "name": "UFCRError",
+            "message": error_dbg,
+            "userMsg": error_msg
+        },
     }))
     .ok();
 }
