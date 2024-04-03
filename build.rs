153386b7638cc1797f5b90d4d1eaa84d3430fa83
Mahesh Bandara Wijerathna <m4heshd@gmail.com>
2024-04-03 12:20:56 +0530

    [build] Set Windows executable resources such as icon and version info

---
+++ build.rs
@@ -0,0 +1,25 @@
+// Libs
+use winresource::WindowsResource;
+
+fn main() {
+    let target = std::env::var("CARGO_CFG_TARGET_OS").unwrap();
+    let profile = std::env::var("PROFILE").unwrap();
+    let exe_res = std::env::var("CARGO_FEATURE_EXE_RES").is_ok();
+
+    // Adds resources to the Windows builds
+    if target == "windows" && profile == "release" && exe_res {
+        println!("cargo:warning=Adding Windows build resources");
+
+        let mut res = WindowsResource::new();
+
+        res.set_icon("project-res/images/ufc-ripper-icon.ico")
+            .set("ProductName", "UFC Ripper")
+            .set("FileDescription", "UFC Ripper")
+            .set(
+                "LegalCopyright",
+                "Copyright © 2024 Mahesh Bandara Wijerathna (m4heshd)",
+            )
+            .compile()
+            .unwrap();
+    }
+}
